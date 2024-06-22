d112aa619321d7c69776909e0f825133d36da783
blonkers <173017178+blonkers@users.noreply.github.com>
2024-06-22 16:14:28 -0400

    fix crashes, add taps only on traded villagers:
    * Fix crashing on _alternative death methods_
    * Only play taps on villagers with level 2+
    * Add job to death notification

--- src/main/java/fun/blonks/mixin/VillagerDeathMixin.java
+++ src/main/java/fun/blonks/mixin/VillagerDeathMixin.java
@@ -6,35 +6,53 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.text.TextContent;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.village.VillagerProfession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VillagerEntity.class)
-public class VillagerDeathMixin {
-
-	private static final Logger log = LoggerFactory.getLogger(VillagerDeathMixin.class);
+public abstract class VillagerDeathMixin {
 
 	@Inject(at = @At("TAIL"), method = "onDeath")
 	private void onDeath(DamageSource source, CallbackInfo ci) {
-		Entity attacker = source.getAttacker();
+		VillagerEntity killed = (VillagerEntity)(Object)this;
+
+		var world = killed.getEntityWorld();
+		MinecraftServer server = world.getServer();
+
 		// Client-side, do nothing
-		if (attacker == null) return;
+		if (world.isClient) return;
 
-		VillagerEntity killed = (VillagerEntity) source.getSource();
-//		TODO: track if traded
-//		if (killed.)
+		var data = killed.getVillagerData();
 
-		var world = attacker.getEntityWorld();
-		var pos = source.getPosition();
-		if (!world.isClient && pos != null) {
+		// Notify on all deaths, for pizzazz
+		if (server != null) {
+			MutableText deathMessage = source.getDeathMessage(killed).copy();
+			String job = data.getProfession() == VillagerProfession.NONE ? "jobless" : data.getProfession().toString();
+			MutableText formattedJob = Text.literal(" (" + job + ")").formatted(Formatting.GOLD);
+			MutableText postedMessage = deathMessage.append(formattedJob);
+			server.getPlayerManager().broadcast(postedMessage, true);
+		}
+
+		// Don't cry over spilled milk
+		if (data.getLevel() <= 1) return;
+
+		var pos = killed.getPos();
+		if (pos != null) {
 			world.playSound(
 				null,
 				pos.x,
@@ -43,12 +61,6 @@ public class VillagerDeathMixin {
 				Taps.TAPS_EVENT,
 				SoundCategory.AMBIENT
 			);
-		}
-
-		MinecraftServer server = world.getServer();
-		if (server != null) {
-			Text deathMessage = source.getDeathMessage(killed);
-			server.getPlayerManager().broadcast(deathMessage, true);
 		}
 	}
 }
