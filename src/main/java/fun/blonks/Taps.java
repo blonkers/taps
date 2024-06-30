9161389f64023fc49c60c241bd87375169c63c32
blonkers <173017178+blonkers@users.noreply.github.com>
2024-06-30 00:22:22 -0400

    support clients without mod installed

--- src/main/java/fun/blonks/Taps.java
+++ src/main/java/fun/blonks/Taps.java
@@ -5,11 +5,15 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
+
+import eu.pb4.polymer.core.api.other.PolymerSoundEvent;
+
 
 public class Taps implements ModInitializer {
 	public static final Identifier TAPS_SOUND_ID = Identifier.of("taps:taps");
-	public static SoundEvent TAPS_EVENT = SoundEvent.of(TAPS_SOUND_ID);
+	public static SoundEvent TAPS_EVENT = PolymerSoundEvent.of(TAPS_SOUND_ID, SoundEvents.ENTITY_VILLAGER_DEATH);
 
 	@Override
 	public void onInitialize() {
